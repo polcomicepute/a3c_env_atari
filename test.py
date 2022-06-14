f284aa38e7e9709adde53aa35a57ec8d91977b5d
이혜민 <polcom@ihyemin-ui-MacBookPro.local>
2022-06-14 15:10:54 +0900

    first

---
+++ test.py
@@ -0,0 +1,93 @@
+import time
+from collections import deque
+
+import torch
+import torch.nn.functional as F
+
+# from envs import create_atari_env
+from envs_hm import make_atari,wrap_deepmind
+
+from model import ActorCritic
+
+
+def test(rank, args, shared_model, counter):
+    torch.manual_seed(args.seed + rank)
+
+    env = make_atari(args.env_name)
+    env = wrap_deepmind(env)
+    env.seed(args.seed + rank)
+    
+    img_h, img_w, img_c = env.observation_space.shape
+    state_size = [1*img_c, img_h, img_w]
+    print('train', state_size)
+
+    model = ActorCritic(state_size[0], env.action_space)
+
+    # model = ActorCritic(env.observation_space.shape[0], env.action_space)
+
+    model.eval()
+
+    state = env.reset()
+    state = torch.from_numpy(state.transpose(2, 0, 1)/255).float()
+
+    reward_sum = 0
+    done = True
+
+    start_time = time.time()
+
+    # a quick hack to prevent the agent from stucking
+    actions = deque(maxlen=100)
+    episode_length = 0
+    
+    while True:
+        episode_length += 1
+        # Sync with the shared model
+        if done:
+            model.load_state_dict(shared_model.state_dict())
+            cx = torch.zeros(1, 256)
+            hx = torch.zeros(1, 256)
+        else:
+            cx = cx.detach()
+            hx = hx.detach()
+
+        with torch.no_grad():
+            value, logit, (hx, cx) = model((state.unsqueeze(0), (hx, cx)))
+        prob = F.softmax(logit, dim=-1)
+        action = prob.max(1, keepdim=True)[1].numpy()
+        
+        
+        # action = prob.multinomial(num_samples=1).detach() #!왜 랜덤하게 샘플링하지? max값이 아니고
+        #     #! num sample개의 인덱스 나옴 
+        # log_prob = log_prob.gather(1, action) #! 해당 action의 인덱스를 logprobd에서 가져와 
+
+        #     #! perform action, get next state and reward
+        #     # print('action',action[0].numpy())
+            
+        # state, reward, done, _ = env.step(int(action[0]))
+        
+
+        state, reward, done, _ = env.step(action[0, 0])
+        env.render()
+        done = done or episode_length >= args.max_episode_length
+        # print(episode_length)
+        reward_sum += reward
+
+        # a quick hack to prevent the agent from stucking
+        actions.append(action[0, 0])
+        # if actions.count(actions[0]) == actions.maxlen:
+        #     done = True
+
+        if done:
+            print("Time {}, num steps {}, FPS {:.0f}, episode reward {}, episode length {}".format(
+                time.strftime("%Hh %Mm %Ss",
+                              time.gmtime(time.time() - start_time)),
+                counter.value, counter.value / (time.time() - start_time),
+                reward_sum, episode_length))
+            reward_sum = 0
+            episode_length = 0
+            actions.clear()
+            state = env.reset()
+            # time.sleep(60)
+
+        state = torch.from_numpy(state.transpose(2, 0, 1)/255).float()
+        
---
+++ train.py
@@ -0,0 +1,125 @@
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+
+# from envs import create_atari_env
+from envs_hm import make_atari,wrap_deepmind
+
+from model import ActorCritic
+
+
+def ensure_shared_grads(model, shared_model):
+    for param, shared_param in zip(model.parameters(),
+                                   shared_model.parameters()):
+        if shared_param.grad is not None:
+            return
+        shared_param._grad = param.grad
+
+
+def train(rank, args, shared_model, counter, lock, optimizer=None):
+    torch.manual_seed(args.seed + rank)
+    
+    
+
+    env = make_atari(args.env_name)
+    env = wrap_deepmind(env)
+    env.seed(args.seed + rank)
+    
+    img_h, img_w, img_c = env.observation_space.shape
+    state_size = [1*img_c, img_h, img_w]
+    print('train', state_size)
+
+    model = ActorCritic(state_size[0], env.action_space)
+
+    if optimizer is None:
+        optimizer = optim.Adam(shared_model.parameters(), lr=args.lr)
+
+    model.train()
+
+    state = env.reset()
+    state = torch.from_numpy(state.transpose(2, 0, 1)/255).float()
+    done = True
+
+    episode_length = 0
+    while True:
+        # Sync with the shared model
+        model.load_state_dict(shared_model.state_dict())
+        if done:
+            cx = torch.zeros(1, 256)
+            hx = torch.zeros(1, 256)
+        else:
+            cx = cx.detach()
+            hx = hx.detach()
+
+        values = []
+        log_probs = []
+        rewards = []
+        entropies = []
+
+        for step in range(args.num_steps):
+            episode_length += 1
+            value, logit, (hx, cx) = model((state.unsqueeze(0),
+                                            (hx, cx)))
+            prob = F.softmax(logit, dim=-1)
+            log_prob = F.log_softmax(logit, dim=-1)
+            entropy = -(log_prob * prob).sum(1, keepdim=True)
+            entropies.append(entropy)
+
+            action = prob.multinomial(num_samples=1).detach() #!왜 랜덤하게 샘플링하지? max값이 아니고
+            #! num sample개의 인덱스 나옴 
+            log_prob = log_prob.gather(1, action) #! 해당 action의 인덱스를 logprobd에서 가져와 
+
+            #! perform action, get next state and reward
+            # print('action',action[0].numpy())
+            
+            state, reward, done, _ = env.step(int(action[0]))
+            # env.render()s
+            done = done or episode_length >= args.max_episode_length
+            reward = max(min(reward, 1), -1)
+
+            with lock:
+                counter.value += 1
+
+            if done:
+                episode_length = 0
+                state = env.reset()
+
+            state = torch.from_numpy(state.transpose(2, 0, 1)/255).float()
+
+            values.append(value)
+            log_probs.append(log_prob)
+            rewards.append(reward)
+
+            if done:
+                break
+
+        R = torch.zeros(1, 1)
+        if not done:
+            value, _, _ = model((state.unsqueeze(0), (hx, cx)))
+            R = value.detach()
+
+        values.append(R)
+        policy_loss = 0
+        value_loss = 0
+        gae = torch.zeros(1, 1)
+        for i in reversed(range(len(rewards))):
+            R = args.gamma * R + rewards[i]
+            advantage = R - values[i]
+            value_loss = value_loss + 0.5 * advantage.pow(2)
+
+            # Generalized Advantage Estimation
+            delta_t = rewards[i] + args.gamma * \
+                values[i + 1] - values[i]
+            gae = gae * args.gamma * args.gae_lambda + delta_t
+
+            policy_loss = policy_loss - \
+                log_probs[i] * gae.detach() - args.entropy_coef * entropies[i]
+
+        optimizer.zero_grad()
+
+        (policy_loss + args.value_loss_coef * value_loss).backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), args.max_grad_norm)
+
+        ensure_shared_grads(model, shared_model)
+        optimizer.step()
